9a5a2fd9698f51b89080cc80d363a166796c74e7
dhruvahari27 <166746756+dhruvahari27@users.noreply.github.com>
2025-01-09 18:23:32 +0000

    source config done

--- p1/test.js
+++ p1/test.js
@@ -1,1 +1,59 @@
-console.log('fhgujdsa');
+                       // object //
+
+ //singlton - constructor se banega to singlton banega  
+
+// objects literal
+
+// const User={
+//     name:"dhruv",
+//     age:"18",
+//     loaction:"udaipur",
+//     email:"dhruvmeena27122004@gamil.com",
+//     isloggedIn:false,
+// }
+//  console.log(User.email);
+//  console.log(User['email']); 
+// //  Object.freeze(User);
+//  User.age="19"; 
+//  console.log(User.age);
+//******************************************** */
+//  const tinderuser= {}  //nonsinglton 
+//      const tinderuser={}
+//  tinderuser.id ="123abc"
+//  tinderuser.name="Sammy"
+//  tinderuser.isloggedIn =false
+//  console.log(tinderuser);
+
+//  const obj1={ 1:"a", 2:"b" ,3:"c"};
+//  const obj2={ 4:"a", 5:"b" ,6:"c"};
+//  const obj3={...obj1 ,...obj2};
+//  console.log(obj3);
+
+// **************************************
+
+// console.log(Object.keys(tinderuser));
+// console.log(Object.values(tinderuser));
+// console.log(Object.entries(tinderuser));
+
+// *************************************
+               //Function //
+  
+ function addtwonumber(num1,num2){
+    console.log(num1+num2);
+ }              
+     addtwonumber(2,4);
+                
+      
+//    *************************************
+
+
+// const course = {
+//     coursename:"js in hindi",
+//     price:"999",
+//     courseInstructor:"hitesh"
+// }
+// console.log(courseInstructor);
+// ***************************************
+
+            //  API
+
